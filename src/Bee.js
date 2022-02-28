69e4915d0b86c370ac12afe4c31dd4fa9b751ff0
Timothy <timnguy17@gmail.com>
2022-02-28 12:29:31 -0800

    pass tests for beesbeesbees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,17 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
+
 };
 
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+//can also inhert methods manually
+Bee.prototype.eat = function() {
+  Grub.eat;
+};
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,23 @@
 var ForagerBee = function() {
+
+  //invoke the superclass constructor (Bee) on the constructor (ForageBee) then use .call to invoke constrctor as a method of the obj
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
+
 };
+
+//allows properties to be inherited, but will remove method to construct new subclasses
+ForagerBee.prototype = Object.create(Grub.prototype);
+//in order to put put functionality to add subclasses back...set the constructor prototype onto itself
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function (treasure) {
+  this.treasureChest.push(treasure);
+};
+
 
